96666e34514fa913f256d8458262b575faf9d8d1
Shriya Dale <shriyadale85@gmail.com>
2025-11-26 19:16:38 -0500

    fixed navbar rerouting

---
+++ views/contact.php
@@ -0,0 +1,24 @@
+<?php require("connect-db.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="CavClubs">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include("header.php") ?>
+    <div class="container mt-4">
+        <h1>Contact</h1>
+        <p>If you have questions about CavClubs, contact the team at <a href="mailto:example@domain.com">example@domain.com</a>.</p>
+        <p>This is a placeholder contact page. Replace with a contact form or details as desired.</p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
